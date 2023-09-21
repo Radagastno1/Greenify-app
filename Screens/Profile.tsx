744c75f3d018e1be600c7d6e765adadbc085f7b5
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-09-21 14:03:37 +0200

    bcakgrundbild på profile och username

--- Screens/Profile.tsx
+++ Screens/Profile.tsx
@@ -1,5 +1,5 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import { RootStackParamList } from "../App";
 import { users } from "../MockedUsers";
 
@@ -13,7 +13,31 @@ export default function ProfileScreen({ route, navigation }: Props) {
 
   return (
     <View>
-      <Text>{thisUser?.username}</Text>
+      <ImageBackground
+        style={styles.backgroundImage}
+        source={{
+          uri: "https://sites.edgehill.ac.uk/sustainnet/files/2020/06/WorldHands.jpg",
+        }}
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            padding: 2,
+            fontSize: 20,
+            backgroundColor: "rgba(247, 226, 237, 0.5)",
+            borderRadius: 10,
+            top: 120,
+          }}
+        >
+          {thisUser?.username}
+        </Text>
+      </ImageBackground>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    height: 200,
+  },
+});
